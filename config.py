3b1b0d3803aa49dffa94e6dfeaad8e7eef7851b2
MaduviMv <156777120+MaduviMv@users.noreply.github.com>
2024-07-05 16:22:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,18 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("7321805924:AAFn0vSb5zJ5wAfpu1-yGjICeMHHja6ks-s", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7321805924:AAFn0vSb5zJ5wAfpu1-yGjICeMHHja6ks-s")
     # The Telegram API things
-    APP_ID = int(os.environ.get("7918878", 12345))
-    API_HASH = os.environ.get("898e389be0e77b4cded80f367447a09c")
+    APP_ID = int(os.environ.get("APP_ID", "7918878"))
+    API_HASH = os.environ.get("API_HASH", "898e389be0e77b4cded80f367447a09c")
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("1168853988", "").split())
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "1168853988").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 2097152000
     # chunk size that should be used with requests
     CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
     # default thumbnail to be used in the videos
@@ -29,4 +28,4 @@ class Config(object):
     # watermark file
     DEF_WATER_MARK_FILE = ""
     #Admin id is stored in 
-    LAZY_DEVELOPER = set(int(x) for x in os.environ.get("1168853988", "").split())
+    LAZY_DEVELOPER = set(int(x) for x in os.environ.get("LAZY_DEVELOPER", "1168853988").split())
